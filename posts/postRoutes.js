69ca4406f19a18173275f39ef45c8ffaa8d98201
Amanda Hogan <ahogan@teamworks.com>
2020-03-10 14:51:43 -0400

    Get post by id

--- posts/postRoutes.js
+++ posts/postRoutes.js
@@ -11,8 +11,8 @@ router.post('/', (req, res) => {
     res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
   } else {
     Posts.insert(req.body)
-    .then(response => {
-      res.status(201).json(response);
+    .then(posts => {
+      res.status(201).json(posts);
     })
     .catch(error => {
       console.log(error);
@@ -22,35 +22,48 @@ router.post('/', (req, res) => {
 });
 
 router.post('/:id/comments', (req, res) => {
-  const id = req.params.id;
-
-  if (!id) {
-    res.status(404).json({ message: "The post with the specified ID does not exist." });
-  } else if (!req.body.text) {
+  if (!req.body.text) {
     res.status(400).json({ errorMessage: "Please provide text for the comment." });
   } else {
-    Posts.findPostComments(id)
-    .then(response => {
-      res.status(201).json(response);
+    Posts.findPostComments(req.params.id)
+    .then(post => {
+      if (post) {
+        res.status(201).json(post);
+      } else {
+        res.status(404).json({ message: "The post with the specified ID does not exist." });
+      };
     })
     .catch(error => {
       console.log(error);
       res.status(500).json({ error: "There was an error while saving the comment to the database" });
     });
-  }
+  };
 });
 
 router.get('/', (req, res) => {
-
-  res.status(200).json();
-  res.status(500).json({ error: "The posts information could not be retrieved." });
+  Posts.find()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ error: "The posts information could not be retrieved." });
+    });
 });
 
 router.get('/:id', (req, res) => {
-
-  res.status(200).json();
-  res.status(404).json({ message: "The post with the specified ID does not exist." });
-  res.status(500).json({ error: "The post information could not be retrieved." });
+  Posts.findById(req.params.id)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: "The post with the specified ID does not exist." });
+      };
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ error: "The post information could not be retrieved." });
+    });
 });
 
 router.get('/:id/comments', (req, res) => {
