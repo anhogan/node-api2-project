3f38be8575e2775439c693ed52ac526cd1d8e7ce
Amanda Hogan <ahogan@teamworks.com>
2020-03-10 14:42:59 -0400

    Logic to post a comment

--- posts/postRoutes.js
+++ posts/postRoutes.js
@@ -22,11 +22,22 @@ router.post('/', (req, res) => {
 });
 
 router.post('/:id/comments', (req, res) => {
+  const id = req.params.id;
 
-  res.status(201).json();
-  res.status(400).json({ errorMessage: "Please provide text for the comment." });
-  res.status(404).json({ message: "The post with the specified ID does not exist." });
-  res.status(500).json({ error: "There was an error while saving the comment to the database" });
+  if (!id) {
+    res.status(404).json({ message: "The post with the specified ID does not exist." });
+  } else if (!req.body.text) {
+    res.status(400).json({ errorMessage: "Please provide text for the comment." });
+  } else {
+    Posts.findPostComments(id)
+    .then(response => {
+      res.status(201).json(response);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ error: "There was an error while saving the comment to the database" });
+    });
+  }
 });
 
 router.get('/', (req, res) => {
