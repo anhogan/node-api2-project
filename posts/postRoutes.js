271f6f7b66c1d47a9f0036d77785cf6be45156fa
Amanda Hogan <ahogan@teamworks.com>
2020-03-10 11:32:39 -0400

    Setup different routes for posts

--- posts/postRoutes.js
+++ posts/postRoutes.js
@@ -2,6 +2,34 @@ const express = require('express');
 
 const router = express.Router();
 
+// Routes chained off of base /api/posts
 
+router.post('/', (req, res) => {
+
+});
+
+router.post('/:id/comments', (req, res) => {
+
+});
+
+router.get('/', (req, res) => {
+
+});
+
+router.get('/:id', (req, res) => {
+
+});
+
+router.get('/:id/comments', (req, res) => {
+
+});
+
+router.delete('/:id', (req, res) => {
+
+});
+
+router.put('/:id', (req, res) => {
+
+});
 
 module.exports = router;
